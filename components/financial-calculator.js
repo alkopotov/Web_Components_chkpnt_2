7b5878ce7c99a9b68ed6b9258ba45ff6a7cda7c1
AlHobb <118611357+alkopotov@users.noreply.github.com>
2023-10-10 17:54:12 +0300

    Ver 1.0.0

--- components/financial-calculator.js
+++ components/financial-calculator.js
@@ -30,41 +30,71 @@ constructor() {
   this.creditInterestInput.value = '1';
 
   this.creditSumInput.addEventListener('input', () => this.handleInput(this.creditSumInput, 1000));
+  this.creditSumInput.addEventListener('blur', () => this.handleBlur(this.creditSumInput, 1000));
   this.creditTermInput.addEventListener('input', () => this.handleInput(this.creditTermInput, 1, true));
-  this.creditInterestInput.addEventListener('input', () => this.handleInput(this.creditInterestInput, 0.5));
+  this.creditTermInput.addEventListener('blur', ()=> this.handleBlur(this.creditTermInput, 1))
+  this.creditInterestInput.addEventListener('input', () => this.handleInput(this.creditInterestInput, 0.1));
+  this.creditInterestInput.addEventListener('blur', ()=> this.handleBlur(this.creditInterestInput, 0.1))
   
   this.creditInterestLabel = document.createElement('label')
   this.creditInterestLabel.innerText = "Ставка по кредиту, %% годовых"
   
-  this.monthlyPaymentField = document.createElement('div')
+  this.monthlyPaymentField = document.createElement('h3')
+  this.totalPaymentField = document.createElement('h3')
+  this.totalInterestField = document.createElement('h3')
   
+  this.wrapper = document.createElement('section')
+  this.wrapper.className = 'wrapper'
 
-  this.calculateCredit();
+  // this.calculateCredit();
   this.linkCSS = document.createElement('link')
   this.linkCSS.rel = 'stylesheet'
   this.linkCSS.href = './components/financial-calculator.css'
  
 }
+  handleBlur(input, minValue) {
 
+    if (input.value.includes(',')) {
+      while (input.value.endsWith('0')){
+        input.value = input.value.slice(0, -1)
+      }
+    }
+    if (input.value.endsWith(',')) {
+      input.value = input.value.slice(0, -1)
+    }
+    if (+input.value.replace(', ', '.').replaceAll(' ', '') < minValue) {
+      input.value = this.formatNumber(minValue) + ''
+      this.calculateCredit()
+    }
+  }
+
+  removeGarbage(str) {
+    return str.split('').filter(elem => '0123456789,'.indexOf(elem) != -1).join('')
+  }
+  
   handleInput(input, minValue, isInteger = false) {
 
-    // Исключаем сброса значений полей ввода ниже минимумов (включая пустые поля ввода)
-    if (!input.value || input.value == '0' || input.value == ',') {
-      input.value = this.formatNumber(minValue) + ''
+    if (input.value == ',' || input.value == '.') {
+      input.value = '0,'
+    }
+
+    if (!input.value || input.value == '0') return
+    if (input.value == '0,' || input.value == '0.') {
+      input.value == input.value.replace(',', '.')
+      return
     }
     
+    
     // Убираем пробелы из значения поля ввода, т.к. отображаем в полях ввода числа с разделителями разрядов
-    let val = input.value.trim().replace(',', '.').replaceAll(' ', '')
-    console.log(val)
+    let val = this.removeGarbage(input.value).replace(',', '.').replaceAll(',', '')
    
     /* Убираем последний введенный нечисловой символ либо пытаемся докопаться до числа,
      если в поле ввода будет скопировано значение, а не введено с клавиатуры, в худшем случае получаем '' */
 
-    while (isNaN(val) && val.length > 0){
-      val = val.slice(0, -1)
-      console.log(val);
+    if (isNaN(val)) {
+      val = this.removeGarbage(val) || minValue+''
     }
-
+    
     if (!isNaN(val)) {  
       if(input.max && +val > input.max) {
         val = input.max + ''
@@ -116,23 +146,24 @@ constructor() {
     this.monthlyPayment = (this.creditSum * this.creditInterest / 12 * ((1 + this.creditInterest / 12) ** this.creditTerm) / (((1 + this.creditInterest/12) ** this.creditTerm)- 1)).toFixed(2)
     this.totalSum = (this.monthlyPayment * this.creditTerm).toFixed(2);
     this.totalInterest = (this.totalSum - this.creditSum).toFixed(2);
-    this.monthlyPaymentField.innerText = this.formatNumber(this.monthlyPayment, ' ', 2)
-    console.log(this.monthlyPayment)
+    this.monthlyPaymentField.innerHTML = `Ежемесячный платеж: <strong class="number_display">${this.formatNumber(this.monthlyPayment, ' ', 2)}</strong>`
+    this.totalPaymentField.innerHTML = `Сумма всех платежей: <strong class="number_display">${this.formatNumber(this.totalSum)}</strong>`
+    this.totalInterestField.innerHTML = `Сумма процентных платежей: <strong class="number_display">${this.formatNumber(this.totalInterest)}</strong>`
+    console.log('Данные обновлены')
   }
   connectedCallback() {
-    console.log(this.creditSum)
-    console.log(this.creditTerm)
-    console.log(this.creditInterest)
-    this.creditSumLabel.append(this.creditSumInput)
-    this.creditTermLabel.append(this.creditTermInput)
-    this.creditInterestLabel.append(this.creditInterestInput)
-    
-    this.formCredit.append(this.linkCSS, this.creditSumLabel, this.creditInterestLabel, this.creditTermLabel)
-    const shadow = this.attachShadow({mode: 'open'})
-    // shadow.adoptedStyleSheets = [this.sheet]
-    shadow.append(this.formCredit, this.monthlyPaymentField)
-    this.calculateCredit()
-    console.log(this.monthlyPayment)
+    this.creditSumLabel.append(this.creditSumInput);
+    this.creditTermLabel.append(this.creditTermInput);
+    this.creditInterestLabel.append(this.creditInterestInput);
+    this.formCredit.append(this.linkCSS, this.creditSumLabel, this.creditInterestLabel, this.creditTermLabel);
+    this.wrapper.append(this.formCredit, this.monthlyPaymentField, this.totalPaymentField, this.totalInterestField)
+    const shadow = this.attachShadow({mode: 'open'});
+    shadow.append(this.wrapper);
+    console.log('Кредитный калькулятор создан');
+    this.calculateCredit();
+  }
+  disconectedCallback() {
+    console.log('Кредитный калькулятор удален из документа')
   }
 }
 
